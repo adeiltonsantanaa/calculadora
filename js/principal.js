1cb63293f61c9502861d11925e6275b8f3c5f54d
adeiltonsantanaa <filhoaguiar20@gmail.com>
2022-03-31 15:44:01 -0300

    primeiro commit

---
+++ js/principal.js
@@ -0,0 +1,36 @@
+function insert(num) {
+    var numero = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = numero + num;
+}
+
+function limpar() {
+    document.getElementById('resultado').innerHTML = "";
+    //document.getElementById('numEscritos').innerHTML = "";
+}
+
+function limparUltimaConta() {
+    document.getElementById('numEscritos').innerHTML = "Último Resultado =";
+}
+
+function ultimoResultado() {
+    var resultado = document.getElementById('resultado').innerHTML;
+    var ultimoResultado = new Array();
+    resultado = ultimoResultado.push(document.getElementById('numEscritos').innerHTML = ('Último Resultado = ' + resultado));
+}
+
+
+function apagarNumEsquerda() {
+    var resultado = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length - 1);
+}
+
+function calcular() {
+    var resultado = document.getElementById('resultado').innerHTML;
+    if (resultado) {
+        document.getElementById('resultado').innerHTML = eval(resultado);
+
+    } else {
+        document.getElementById('resultado').innerHTML = "Digite Alguma Operação Antes!!!";
+
+    }
+}
---
+++ readme.md
@@ -0,0 +1,8 @@
+<strong>CALCULADORA</strong>
+
+Tecnologias Usadas:<br> 
+- HTML<br> 
+- CSS<br> 
+- JavaScript<br> 
+
+Projeto ainda em desenvolvimento...
