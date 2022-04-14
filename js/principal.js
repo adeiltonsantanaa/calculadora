f41fd9ceaede6da0900b664f2045ee9e8164ca19
ADEILTON SANTANA AGUIAR FILHO <202108672157@alunos.estacio.br>
2022-04-14 09:55:04 -0300

    Adicionanando a função de apagar tudo

--- js/principal.js
+++ js/principal.js
@@ -7,6 +7,10 @@ function limpar() {
     document.getElementById('resultado').innerHTML = "";
     //document.getElementById('numEscritos').innerHTML = "";
 }
+function limpartudo() {
+    document.getElementById('resultado').innerHTML = "";
+    document.getElementById('numEscritos').innerHTML = "Último Resultado =";
+}
 
 function limparUltimaConta() {
     document.getElementById('numEscritos').innerHTML = "Último Resultado =";
